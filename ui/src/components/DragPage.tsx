c0aab2ba00b15310af2f613d0245c9337edbaa6f
firstclimbkazu <firstclimbkazu@gmail.com>
2019-07-27 03:44:31 +0900

    stateビルドエラー解消

--- ui/src/components/DragPage.tsx
+++ ui/src/components/DragPage.tsx
@@ -4,22 +4,46 @@ import Draggable from 'react-draggable';
 
 class DragPage extends Component {
 
+    constructor(props: any) {
+        super(props);
+        const x = localStorage.getItem("position_x");
+        const y = localStorage.getItem("position_y");
+        this.state = {
+            componentPosition: {
+                x: Number(x) || 0,
+                y: Number(y) || 0
+            }
+        };
+    };
+
+    onDrag = (e: MouseEvent, position: any) => {
+        const { x, y } = position;
+        this.setState({
+            position: { x, y }
+        });
+    };
+
+    onStop = (e: MouseEvent, position: any) => {
+        const { x, y } = position;
+
+        localStorage.setItem("position_x", x);
+        localStorage.setItem("position_y", y)
+    };
+
     eventLogger = (e: MouseEvent, data: Object) => {
         console.log('Event: ', e);
         console.log('Data: ', data);
     };
+
     render() {
         return (
             <Draggable
                 axis="x"
                 handle=".handle"
                 defaultPosition={{ x: 0, y: 0 }}
-                position={{ x: 0, y: 0 }}
+                position={this.state}
                 grid={[25, 25]}
                 scale={1}
-            //onStart={this.handleStart}
-            //onDrag={this.handleDrag}
-            //onStop={this.handleStop}
             >
                 <div>
                     <div className="handle">Drag from here</div>
